b45d750516695bd43c861713ba12753e2cf31a7f
Luz-dmp <luzmendezpenate@gmail.com>
2024-11-21 16:44:30 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
+
 const menuIcono = document.getElementById('menu-icono');
 const navbar = document.getElementById('navbar');
+
 
 menuIcono.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
 
+
 const formulario = document.querySelector('form');
 
 formulario.addEventListener('submit', (event) => {
-    event.preventDefault(); 
+    event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
 
-    
-    
     const nombre = document.getElementById('nombre').value;
     const email = document.getElementById('email').value;
     const telefono = document.getElementById('telefono').value;
-    const voluntariado = document.getElementById('voluntariado').value;
-
 
     if (nombre === '' || email === '' || telefono === '') {
         alert('Por favor, completa todos los campos.');
         return;
     }
 
-  
+    
     console.log(`Formulario enviado:
     Nombre: ${nombre}
     Correo: ${email}
-    Teléfono: ${telefono}
-    Voluntariado seleccionado: ${voluntariado}`);
+    Teléfono: ${telefono}`);
 
-
+   
     alert('¡Gracias por tu inscripción! Nos pondremos en contacto contigo pronto.');
 
-  
-    formulario.reset();
+    formulario.reset(); 
 });
