a8e229e234e67c112251383e1370dd92a3f357df
Luz-dmp <luzmendezpenate@gmail.com>
2024-11-22 18:12:56 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const menuIcono = document.getElementById('menu-icono');
 const navbar = document.getElementById('navbar');
 
-
 menuIcono.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
@@ -22,28 +21,27 @@ formulario.addEventListener('submit', (event) => {
         return;
     }
 
- 
+     
     const datosGuardados = localStorage.getItem('usuario');
     if (datosGuardados) {
         const usuarioExistente = JSON.parse(datosGuardados);
         if (usuarioExistente.email === email) {
-            alert('Ya tienes una cuenta registrada. Por favor, inicia sesión.');
-            window.location.href = "gracias.html"; // Redirigir a página de Gracias con opción de iniciar sesión
+            // Redirigir directamente a la página de "Gracias"
+            window.location.href = "gracias.html";
             return;
         }
     }
 
-  
     const datosUsuario = {
         nombre: nombre,
         email: email,
         telefono: telefono,
     };
 
-  
+ 
     localStorage.setItem('usuario', JSON.stringify(datosUsuario));
 
    
-    alert('¡Gracias por registrarte! Redirigiendo a la página de inicio.');
+
     window.location.href = "gracias.html";
 });
